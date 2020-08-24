b683d008d0439717227fde12f19c84ad6c52504a
belogod <belogod@gmail.com>
2020-08-24 16:59:28 +0300

    save from anx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,38 +19,71 @@ export class AppComponent {
 
 
   onDown(type: string) {
-    this.text = '<breakfast_menu>\n' +
-      '<food>\n' +
-      '<name>Belgian Waffles</name>\n' +
-      '<price>$5.95</price>\n' +
-      '<description>Two of our famous Belgian Waffles with plenty of real maple syrup</description>\n' +
-      '<calories>650</calories>\n' +
-      '</food>\n' +
-      '<food>\n' +
-      '<name>Strawberry Belgian Waffles</name>\n' +
-      '<price>$7.95</price>\n' +
-      '<description>Light Belgian waffles covered with strawberries and whipped cream</description>\n' +
-      '<calories>900</calories>\n' +
-      '</food>\n' +
-      '<food>\n' +
-      '<name>Berry-Berry Belgian Waffles</name>\n' +
-      '<price>$8.95</price>\n' +
-      '<description>Light Belgian waffles covered with an assortment of fresh berries and whipped cream</description>\n' +
-      '<calories>900</calories>\n' +
-      '</food>\n' +
-      '<food>\n' +
-      '<name>French Toast</name>\n' +
-      '<price>$4.50</price>\n' +
-      '<description>Thick slices made from our homemade sourdough bread</description>\n' +
-      '<calories>600</calories>\n' +
-      '</food>\n' +
-      '<food>\n' +
-      '<name>Homestyle Breakfast</name>\n' +
-      '<price>$6.95</price>\n' +
-      '<description>Two eggs, bacon or sausage, toast, and our ever-popular hash browns</description>\n' +
-      '<calories>950</calories>\n' +
-      '</food>\n' +
-      '</breakfast_menu>';
+    this.text =
+      '<Chart GridHeightSize="10.952756e-01" UseDefaultLinkSpacingWhenDragging="false" GridVisibleOnAllViews="true" ShowAllFlag="false" ShowPages="true" BackColour="16777215" DefaultLinkSpacing="2.952756e-01" WiringDistanceNear="7.874016e-02" UseWiringHeightForThemeIcon="true" DefaultTickRate="1.000000e+00" WiringHeight="1.968504e-01" GridWidthSize="2.952756e-01" HideMatchingTimeZoneFormat="false" TimeBarVisible="false" WiringSpacing="1.968504e-01" BlankLinkLabels="false" UseLocalTimeZone="true" WiringDistanceFar="3.937008e-01" IdReferenceLinking="false" SnapToGrid="false" LabelSumNumericLinks="false" LabelRule="LabelRuleMerge" Rigorous="true" MsxmlVersion="4.20.9818.0" SchemaVersion="7.0.0.1">\n' +
+        '<EntityTypeCollection>\n' +
+          '<EntityType Name="Person" Colour="0" IconFile="Person" PreferredRepresentation="RepresentAsIcon"/>\n' +
+          '<EntityType Name="Woman" Colour="0" IconFile="Phone" PreferredRepresentation="RepresentAsIcon"/>\n' +
+        '</EntityTypeCollection>\n' +
+        '<LinkTypeCollection>\n' +
+          '<LinkType Colour="0" Name="Link"/>\n' +
+        '</LinkTypeCollection>\n' +
+        '<ChartItemCollection>\n' +
+          '<ChartItem DateSet="false" GradeOneIndex="0" GradeTwoIndex="0" Selected="false" GradeThreeIndex="0" TimeSet="false" Ordered="false" Label="τττ" XPosition="158">\n' +
+            '<End Y="162" X="158" Z="0">\n' +
+              '<Entity EntityId="n0" Identity="Tyrion" LabelIsIdentity="false">\n' +
+                '<Icon TextX="0" TextY="16">\n' +
+                  '<IconStyle Type="Person"/>\n' +
+                '</Icon>\n' +
+              '</Entity>\n' +
+            '</End>\n' +
+          '</ChartItem>\n' +
+          '<ChartItem DateSet="false" GradeOneIndex="0" GradeTwoIndex="0" Selected="false" GradeThreeIndex="0" TimeSet="false" Ordered="false" Label="Tywin" XPosition="211">\n' +
+            '<End Y="0" X="211" Z="0">\n' +
+              '<Entity EntityId="n1" Identity="Tywin" LabelIsIdentity="false">\n' +
+                '<Icon TextX="0" TextY="16">\n' +
+                  '<IconStyle Type="Person"/>\n' +
+                '</Icon>\n' +
+              '</Entity>\n' +
+            '</End>\n' +
+          '</ChartItem>\n' +
+          '<ChartItem DateSet="false" GradeOneIndex="0" GradeTwoIndex="0" Selected="false" GradeThreeIndex="0" TimeSet="false" Ordered="false" Label="Jaime" XPosition="0">\n' +
+            '<End Y="216" X="0" Z="0">\n' +
+              '<Entity EntityId="n2" Identity="Jaime" LabelIsIdentity="false">\n' +
+                '<Icon TextX="0" TextY="16">\n' +
+                  '<IconStyle Type="Person"/>\n' +
+                '</Icon>\n' +
+              '</Entity>\n' +
+            '</End>\n' +
+          '</ChartItem>\n' +
+          '<ChartItem DateSet="false" GradeOneIndex="0" GradeTwoIndex="0" Selected="false" GradeThreeIndex="0" TimeSet="false" Ordered="false" Label="Cersei" XPosition="0">\n' +
+            '<End Y="0" X="0" Z="0">\n' +
+              '<Entity EntityId="n3" Identity="Cersei" LabelIsIdentity="false">\n' +
+                '<Icon TextX="0" TextY="16">\n' +
+                  '<IconStyle Type="Woman"/>\n' +
+                '</Icon>\n' +
+              '</Entity>\n' +
+            '</End>\n' +
+          '</ChartItem>\n' +
+        '</ChartItemCollection>\n' +
+        '<ChartItemCollection>\n' +
+          '<ChartItem DateSet="false" GradeOneIndex="0" GradeTwoIndex="0" Selected="false" GradeThreeIndex="0" TimeSet="false" Ordered="false" Label="Father of" XPosition="0">\n' +
+            '<Link End1Id="n1" LabelSegment="0" LabelPos="50" End2Id="n0">\n' +
+              '<LinkStyle ArrowStyle="ArrowOnHead" Type="Link"/>\n' +
+            '</Link>\n' +
+          '</ChartItem>\n' +
+          '<ChartItem DateSet="false" GradeOneIndex="0" GradeTwoIndex="0" Selected="false" GradeThreeIndex="0" TimeSet="false" Ordered="false" Label="Brother of" XPosition="0">\n' +
+            '<Link End1Id="n2" LabelSegment="0" LabelPos="50" End2Id="n0">\n' +
+              '<LinkStyle ArrowStyle="ArrowOnHead" Type="Link"/>\n' +
+            '</Link>\n' +
+          '</ChartItem>\n' +
+          '<ChartItem DateSet="false" GradeOneIndex="0" GradeTwoIndex="0" Selected="false" GradeThreeIndex="0" TimeSet="false" Ordered="false" Label="25/13" XPosition="0">\n' +
+            '<Link End1Id="n3" LabelSegment="0" LabelPos="50" End2Id="n0">\n' +
+              '<LinkStyle ArrowStyle="ArrowOnHead" Type="Link"/>\n' +
+            '</Link>\n' +
+          '</ChartItem>\n' +
+        '</ChartItemCollection>\n' +
+      '</Chart>';
     const fileName = `save.${type}`;
     const fileType = this.fileSaverService.genType(fileName);
     const txtBlob = new Blob([this.text], {type: fileType});
